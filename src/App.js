e9a9b66225c58c3b4c5863cccc77a12b384cfca7
tofa <andronova-arina@rambler.ru>
2025-07-30 14:34:49 +0300

    шапка + начало мейна

--- src/App.js
+++ src/App.js
@@ -1,79 +1,47 @@
-import logo from './logo.svg';
+import educationImg from './assets/Education.png'
+import linkImg from './assets/Link.png'
 import './App.css';
 import { v4 } from 'uuid'
 import fetch from 'isomorphic-fetch'
 import { Component } from 'react';
 
-/*
-class AppClass extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      colors: [
-        {
-          "id": "1",
-          "title": "Klein Blue",
-          "color": "#002FA7",
-          "rating": "0"
-        },
-        {
-          "id": "f9005b4e-975e-433d-a646-79df172e1dbb",
-          "title": "Tomato",
-          "color": "#d10012",
-          "rating": 2
-        },
-      ]
-    };
-    this.addColor = this.addColor.bind(this);
-    this.rateColor = this.rateColor.bind(this);
-    this.removeColor = this.removeColor.bind(this);
-
-  }
-
-  addColor(title, color){
-    colors = [...this.state.colors, {title, color, id: v4(), rating: 0}];
-    this.setState({colors});
-  }
-
-  rateColor(id, rating){
-    colors =this.state.colors.map(color => 
-      color.id == id ? {...color, rating} : color
-    );
-    this.setState({colors});
-  }
-
-  removeColor(id){
-    colors = this.state.colors.filter( color => color.id!==id);
-    this.setState({colors})
-  }
-
-  render(){
-    const {addColor, removeColor, rateColor} = this;
-    const {colors} = this.state;
-
-    return (
-      <div className='app'>
-        <AddColorForm onNewColor={addColor} />
-        <ColorList colors={colors}
-        onRate={rateColor}
-        onRemove={removeColor} />
-      </div>
-    )
-  }
-}  */
-
+import DarkVeil from './modules/DarkVeil.js';
+import Navbar from './modules/Navbar.js'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav className="nav-header"> 
-          <li>Прив</li>
-          <li>Обо мне</li>
-          <li>Образование</li>
-          <li>Достижение</li>
-          <li>Проекты</li> 
-        </nav>
+    <div className='App'>
+      <header className='App-header'>
+        <DarkVeil hueShift={16} speed={1}/>
+        <Navbar/>
+        <div className='text'>
+          <p>Меня зовут <span>Арина</span></p>
+          <p>Я <span>Frontend</span> разработчик</p>
+        </div>
       </header>
+      
+      <main>
+        <div className='App-main-div'>
+          <div className='div-title'>
+            <p>Обо мне</p>
+          </div>
+          <div className='div-boxes'>
+            <div className='grid-child'> 
+              <div className='title'> <img src={educationImg}/> Образование </div>
+              <div className='text'> <p> Учусь на 3 курсе РТУ МИРЭА по направлению Фуллстэк-разработка </p></div>
+            </div>
+
+            <div className='grid-child'> f</div>
+            <div className='grid-child big'> sf</div>
+          </div>
+          
+        </div>
+
+
+      </main>
+      
+      <footer>
+
+      </footer>
     </div>
   );
 }
---
+++ src/modules/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar(){
+    return(
+        <div className="nav-header-div">
+            <nav className="nav-header"> 
+                <li>Прив</li>
+                <li>Обо мне</li>
+                <li>Достижение</li>
+                <li>Проекты</li> 
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
