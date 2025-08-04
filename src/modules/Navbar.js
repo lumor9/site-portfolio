8ecd3ce10d2f62a5dead0932851889cfec276576
tofa <andronova-arina@rambler.ru>
2025-08-04 22:44:56 +0300

    адаптивная навигационная панель

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -1,14 +1,27 @@
 import './Navbar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import menuImg from '../assets/Menu.png'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 function Navbar(){
     return(
-        <div className="nav-header-div">
-            <nav className="nav-header"> 
-                <li><a href='#hello'>Привет</a></li>
-                <li><a href='#aboutMe'>Обо мне</a></li>
-                <li><a href='#achievements'>Достижения</a></li>
-                <li><a href='#projects'>Проекты</a></li> 
+        <div className="nav-header-div col-lg-7 col-md-9 col-sm-12">
+            <nav className="navbar navbar-expand-sm px-0">
+                <div className="container-fluid justify-content-center">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav">
+                        <img src={menuImg} alt="Меню"/>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="nav-header ms-auto navbar-nav flex-sm-row flex-column justify-content-space-around">
+                            <li><a className='px-3' href='#hello'>Привет</a></li>
+                            <li><a className='px-3' href='#aboutMe'>Обо мне</a></li>
+                            <li><a className='px-3' href='#achievements'>Достижения</a></li>
+                            <li><a className='px-3' href='#projects'>Проекты</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
             </nav>
         </div>
     )
